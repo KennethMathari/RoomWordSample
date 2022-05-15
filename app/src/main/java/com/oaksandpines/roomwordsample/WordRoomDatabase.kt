2bce591a64ba29db3fc97e040687279452a79ce3
Kenneth Mathari <mathari580@gmail.com>
2022-05-15 17:14:47 +0300

    Initial commit

---
+++ app/src/main/java/com/oaksandpines/roomwordsample/WordRoomDatabase.kt
@@ -0,0 +1,68 @@
+package com.oaksandpines.roomwordsample
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    var wordDao = database.wordDao()
+
+                    // Delete all content here.
+                    wordDao.deleteAll()
+
+                    // Add sample words.
+                    var word = Word("Hello")
+                    wordDao.insert(word)
+                    word = Word("World!")
+                    wordDao.insert(word)
+
+                    // TODO: Add your own words!
+                    word = Word("TODO!")
+                    wordDao.insert(word)
+                }
+            }
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): WordRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/oaksandpines/roomwordsample/WordsApplication.kt
@@ -0,0 +1,11 @@
+package com.oaksandpines.roomwordsample
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class WordsApplication: Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
